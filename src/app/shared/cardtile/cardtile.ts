9bbb5cb1fbfa5f44a053d9c9e5df02ec0c4c2f2d
rylandckclark <rylandckclark@outlook.com>
2025-10-29 11:56:10 -0600

    Updating constructor for typescript card tile. Inputs treated as actual inputs

--- src/app/shared/cardtile/cardtile.ts
+++ src/app/shared/cardtile/cardtile.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { Component, Input} from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
-export interface Card{
+export interface Card {
   Icon: string;
   Title: string;
   Subtitle: string;
@@ -11,14 +11,26 @@ export interface Card{
 @Component({
   selector: 'app-cardtile',
   standalone: true,
-  imports: [
-    MatCardModule,
-  ],
+  imports: [MatCardModule],
   templateUrl: './cardtile.html',
   styleUrl: './cardtile.scss',
 })
 export class Cardtile {
+  
+  @Input() icon: string = "https://i.imgur.com/AxgaNX3.png";
+  @Input() title: string = "Project Showcase";
+  @Input() subtitle: string = "Console BlackJack";
+  @Input() image: string = "https://repository-images.githubusercontent.com/240996786/1d5bce00-87a9-11ea-8e84-f5f04f84c834";
+  @Input() desc: string = "A simple text-input version of the classic Blackjack card game! Written in C# as a .Net Framework Console application, this project is equipped with logic surrounding all possible decisions including hit, stand, split, double-down, surrender, insurance, and any combination of the six. Hand information is packed together in structs and passed between methods to seamlessly deliver game outcomes. Although lacking in visual spectacle, the project serves as an intuitive way to implement switch case statements, structs, loop logic, methods, method overloads, enums, input-checking, delays, and algorithms.";
+
+  /* Card is passed constructor arguments passed in through html*/
   Cards: Card[] = [
-    {Icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRokxV29a-kmKkVDVa3IItgEgQvnTae0bRNnA&s', Title: 'Project Showcase', Subtitle: 'Console BlackJack', Image: 'https://i.imgur.com/AxgaNX3.png', Desc: 'A simple text-input version of the classic Blackjack card game! Written in C# as a .Net Framework Console application, this project is equipped with logic surrounding all possible decisions including hit, stand, split, double-down, surrender, insurance, and any combination of the six. Hand information is packed together in structs and passed between methods to seamlessly deliver game outcomes. Although lacking in visual spectacle, the project serves as an intuitive way to implement switch case statements, structs, loop logic, methods, method overloads, enums, input-checking, delays, and algorithms.'}
-  ]
+    {
+      Icon: this.icon,
+      Title: this.title,
+      Subtitle: this.subtitle,
+      Image: this.image,
+      Desc: this.desc
+    }
+  ];
 }
