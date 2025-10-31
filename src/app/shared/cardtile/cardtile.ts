8c1764848e3a42e90ab1f320c4e7e33152ab8814
rylandckclark <rylandckclark@outlook.com>
2025-10-31 16:11:10 -0600

    Uploading image for project showcase

--- src/app/shared/cardtile/cardtile.ts
+++ src/app/shared/cardtile/cardtile.ts
@@ -13,15 +13,15 @@ export interface Card {
   standalone: true,
   imports: [MatCardModule],
   templateUrl: './cardtile.html',
-  styleUrl: './cardtile.scss',
+  styleUrls: ['./cardtile.scss'],
 })
 export class Cardtile {
   
   @Input() icon: string = "https://i.imgur.com/AxgaNX3.png";
-  @Input() title: string = "Project Showcase";
-  @Input() subtitle: string = "Console BlackJack";
-  @Input() image: string = "https://repository-images.githubusercontent.com/240996786/1d5bce00-87a9-11ea-8e84-f5f04f84c834";
-  @Input() desc: string = "A simple text-input version of the classic Blackjack card game! Written in C# as a .Net Framework Console application, this project is equipped with logic surrounding all possible decisions including hit, stand, split, double-down, surrender, insurance, and any combination of the six. Hand information is packed together in structs and passed between methods to seamlessly deliver game outcomes. Although lacking in visual spectacle, the project serves as an intuitive way to implement switch case statements, structs, loop logic, methods, method overloads, enums, input-checking, delays, and algorithms.";
+  @Input() title: string = 'Project Showcase';
+  @Input() subtitle: string = 'Console BlackJack';
+  @Input() image: string = 'assets/images/blackjack.png';
+  @Input() desc: string = 'A simple text-input version of the classic Blackjack card game! Written in C# as a .Net Framework Console application, this project is equipped with logic surrounding all possible decisions including hit, stand, split, double-down, surrender, insurance, and any combination of the six. Hand information is packed together in structs and passed between methods to seamlessly deliver game outcomes. Although lacking in visual spectacle, the project serves as an intuitive way to implement switch case statements, structs, loop logic, methods, method overloads, enums, input-checking, delays, and algorithms.';
 
   /* Card is passed constructor arguments passed in through html*/
   Cards: Card[] = [
